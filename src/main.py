bd38a4d23e1055cd564b4ff1c27bd3e7b50b06f5
Etienne Jacob <etin.jacob@gmail.com>
2019-05-02 10:43:02 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -38,14 +38,13 @@ if __name__ == "__main__":
     # Model
     model = CleanU_Net(in_channels=1, out_channels=2)
     #model = CleanU_Net()
-    model = torch.nn.DataParallel(model, device_ids=list(
-        range(torch.cuda.device_count()))).cuda()
+    #model = torch.nn.DataParallel(model, device_ids=list(range(torch.cuda.device_count()))).cuda()
 
     # Loss function
     criterion = nn.CrossEntropyLoss()
 
     # Optimizerd
-    optimizer = torch.optim.RMSprop(model.module.parameters(), lr=0.001)
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=0.001)
 
     # Parameters
     epoch_start = 0
